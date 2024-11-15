ef9e55b2e01f69c73541647bb99d5463ae749071
fhuang <fhuang@takima.fr>
2024-11-15 15:18:38 +0100

    Feat(App): Add a selectbox

--- App.py
+++ App.py
@@ -5,6 +5,14 @@ import streamlit as st
 data = {'a': [1, 2, 3], 'b': [4, 5, 6], 'c': [7, 8, 9]}
 df = pd.DataFrame(data)
 
+with st.sidebar:
+    currentTheme = st.selectbox(
+        "Que souhaitez-vous réviser ?",
+        ["SQL", "Spark", "Scala"],
+        placeholder="Sélectionner un thème"
+    )
+    st.write(f"Thème actuel : {currentTheme}")
+
 st.write("df :", df)
 
 client_query = st.text_area(label="Request to apply on this dataframe")
